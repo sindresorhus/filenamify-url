607ca4aa97d420137ef4cd7a1439bc92ffa7d0d8
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 13:59:17 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,14 +1,12 @@
-'use strict';
-var test = require('ava');
-var filenamifyUrl = require('./');
+import test from 'ava';
+import m from './';
 
-test(function (t) {
-	t.assert(filenamifyUrl('sindresorhus.com') === 'sindresorhus.com');
-	t.assert(filenamifyUrl('sindresorhus.com/foo/bar?foo=bar&baz=faz') === 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
-	t.assert(filenamifyUrl('sindresorhus.com/foo/bar?foo=bar&baz=faz') === 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
-	t.assert(filenamifyUrl('http://user:pass@www.sindresorhus.com/foo/bar/') === 'sindresorhus.com!foo!bar');
-	t.assert(filenamifyUrl('http://user@sindresorhus.com') === 'sindresorhus.com');
-	t.assert(filenamifyUrl('sindresorhus.com/foo', {replacement: '🐴'}) === 'sindresorhus.com🐴foo');
-	t.assert(filenamifyUrl('http://www.sindresorhus.com/?query=pageres*|<>:"\\') === 'sindresorhus.com!query=pageres');
-	t.end();
+test(t => {
+	t.is(m('sindresorhus.com'), 'sindresorhus.com');
+	t.is(m('sindresorhus.com/foo/bar?foo=bar&baz=faz'), 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
+	t.is(m('sindresorhus.com/foo/bar?foo=bar&baz=faz'), 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
+	t.is(m('http://user:pass@www.sindresorhus.com/foo/bar/'), 'sindresorhus.com!foo!bar');
+	t.is(m('http://user@sindresorhus.com'), 'sindresorhus.com');
+	t.is(m('sindresorhus.com/foo', {replacement: '🐴'}), 'sindresorhus.com🐴foo');
+	t.is(m('http://www.sindresorhus.com/?query=pageres*|<>:"\\'), 'sindresorhus.com!query=pageres');
 });
