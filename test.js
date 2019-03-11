af7f99f9b94ce44c02c70428019d8b4da87fc806
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-11 03:20:59 +0000

    Require Node.js 8, add TypeScript definition, update dependencies (#3)

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 import test from 'ava';
-import m from './';
+import filenamifyUrl from '.';
 
-test(t => {
-	t.is(m('sindresorhus.com'), 'sindresorhus.com');
-	t.is(m('sindresorhus.com/foo/bar?foo=bar&baz=faz'), 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
-	t.is(m('sindresorhus.com/foo/bar?foo=bar&baz=faz'), 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
-	t.is(m('http://user:pass@www.sindresorhus.com/foo/bar/'), 'sindresorhus.com!foo!bar');
-	t.is(m('http://user@sindresorhus.com'), 'sindresorhus.com');
-	t.is(m('sindresorhus.com/foo', {replacement: '🐴'}), 'sindresorhus.com🐴foo');
-	t.is(m('http://www.sindresorhus.com/?query=pageres*|<>:"\\'), 'sindresorhus.com!query=pageres');
+test('main', t => {
+	t.is(filenamifyUrl('sindresorhus.com'), 'sindresorhus.com');
+	t.is(filenamifyUrl('sindresorhus.com/foo/bar?foo=bar&baz=faz'), 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
+	t.is(filenamifyUrl('sindresorhus.com/foo/bar?foo=bar&baz=faz'), 'sindresorhus.com!foo!bar!baz=faz&foo=bar');
+	t.is(filenamifyUrl('http://user:pass@www.sindresorhus.com/foo/bar/'), 'sindresorhus.com!foo!bar');
+	t.is(filenamifyUrl('http://user@sindresorhus.com'), 'sindresorhus.com');
+	t.is(filenamifyUrl('sindresorhus.com/foo', {replacement: '🐴'}), 'sindresorhus.com🐴foo');
+	t.is(filenamifyUrl('http://www.sindresorhus.com/?query=pageres*|<>:"\\'), 'sindresorhus.com!query=pageres!%7C%3C%3E%3A%22%5C');
 });
